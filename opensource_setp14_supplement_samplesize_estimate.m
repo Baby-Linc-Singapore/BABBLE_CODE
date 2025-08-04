4a7eb47ff27de7632224e91fad1d828d28ca3f98
WilsonNTU2023 <126048974+WilsonNTU2023@users.noreply.github.com>
2025-08-04 13:53:45 +0800

    Update and rename opensource_sup_samplesize_estimate.m to opensource_setp14_supplement_samplesize_estimate.m

--- opensource_setp14_supplement_samplesize_estimate.m
+++ opensource_setp14_supplement_samplesize_estimate.m
@@ -1,12 +1,8 @@
 %% Sample Size Estimation Script
-% NOTE: This code demonstrates the analytical methodology only. Due to data privacy requirements,
-% all data paths, variable names, and values shown here are examples only.
-% The actual analysis was performed on protected research data. Data are protected and are not 
-% available due to data privacy regulations. Access to anonymized data collected can be requested 
-% by contacting the corresponding author (Prof. Victoria Leong, VictoriaLeong@ntu.edu.sg) and 
-% is subject to the establishment of a specific data sharing agreement between the applicant's 
-% institution and the institutions of data collection.
-
+% All datasets have been made publicly available through Nanyang Technological University (NTU)'s 
+% data repository (DR-NTU Data https://researchdata.ntu.edu.sg/) and can be accessed according to 
+% NTU's open access policy.
+%
 % Purpose: Calculate required sample size for detecting a specified effect size
 % with desired statistical power in an experimental design
 % 
